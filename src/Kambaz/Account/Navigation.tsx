139ccbf6c34728ded33f268bff6a9f4e6aef90e7
amyb25 <baez.am@northeastern.edu>
2025-03-12 21:12:19 -0400

    remove unused

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  //const { currentUser } = useSelector((state: any) => state.accountReducer);
+  //const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   return (
     <div id="wd-account-navigation">
       <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br/>
